9743f6df4cce5f455a69144f79ab70a886a23151
Alex Andres Porcel Aguirre <xxwauxxhs2@gmail.com>
2020-10-17 10:36:05 -0500

    Mi primer commit  Please enter the commit message for your changes. Lines starting

---
+++ prueba.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="uft-8">
+    <meta https-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>VUE.JS</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
+</head>
+<body id="app">
+    <h1 id="title">Bienvenido, {{ name }}</h1>
+
+    <input type="text" v-model="name" class="form-control">
+    <hr>
+    <pre>{{ $data | json }}</pre>
+    
+    <script type="text/javascript">
+
+        new Vue({
+            el: "#app",
+            data: {
+                name: "Alex"
+            }
+        });
+
+    </script>
+</body>
+</html>
